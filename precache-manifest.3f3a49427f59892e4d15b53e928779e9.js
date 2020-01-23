f28e91aa26848164b3badcbabac4cec07b839d3a
Tevin Sharp <sharp@Tevins-MacBook-Air-7.local>
2020-01-22 22:06:29 -0600

    Updates

---
+++ precache-manifest.3f3a49427f59892e4d15b53e928779e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae745e4adc081d3203ec7f1356e9da02",
+    "url": "/readbooks/index.html"
+  },
+  {
+    "revision": "3dfe965efb8f1db40d66",
+    "url": "/readbooks/static/css/2.23be29d4.chunk.css"
+  },
+  {
+    "revision": "b5c80eaee0e5fe198ca5",
+    "url": "/readbooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3dfe965efb8f1db40d66",
+    "url": "/readbooks/static/js/2.d30adca5.chunk.js"
+  },
+  {
+    "revision": "d57e69aeadc1d65039037330bbaabf32",
+    "url": "/readbooks/static/js/2.d30adca5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5c80eaee0e5fe198ca5",
+    "url": "/readbooks/static/js/main.dfec94d1.chunk.js"
+  },
+  {
+    "revision": "b1571d9ab16bf028cafe",
+    "url": "/readbooks/static/js/runtime-main.3ddecc84.js"
+  }
+]);
